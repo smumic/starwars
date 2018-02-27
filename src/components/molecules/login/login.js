aba418e1be438baa7c3994160323afb05c765e9f
Samir Mumic <mr.samir.mumic@gmail.com>
2018-02-27 22:39:38 +0100

    adds login styles and functionalty

--- src/components/molecules/login/login.js
+++ src/components/molecules/login/login.js
@@ -5,15 +5,46 @@ import './css/login.css';
 class Login extends Component {
   constructor() {
     super();
+    this.state = {
+      username: "",
+      password: ""
+    };
+  }
+
+  handleUsername = ev => {
+    this.setState({
+      username: ev.target.value
+    });
+    this.props.onTypedUsername(ev.target.value);
+  }
+
+  handlePassword = ev => {
+    this.setState({
+      username: ev.target.value
+    });
+    this.props.onTypedPassword(ev.target.value);
   }
 
   render(){
     return (
       <div className="m-login m-login__container">
         <div className="m-login__header">
-            <h1>Starwars API Login</h1>
+            <h1 className="m-login__title">Login - Starwars API</h1>
         </div>
         <div className="m-login__body">
+            <form className="m-login__form">
+                <div className="m-login__form-container">
+                  <div className="m-login__form-container--child">
+                    <label className="m-login__label"><span className="m-login__icon icon-user"></span>Username</label>
+                    <input className="m-login__input" onChange={this.handleUsername} />
+                  </div>
+                  <div className="m-login__form-container--child">
+                    <label className="m-login__label"><span className="m-login__icon icon-key"></span>Password</label>
+                    <input className="m-login__input" type="password" onChange={this.handlePassword} />
+                  </div>
+                    <button className="m-login__button" onClick={this.props.onLogin}>LOGIN</button>
+                </div>
+            </form>
         </div>
       </div>
     );
--- src/components/organisms/app/app.js
+++ src/components/organisms/app/app.js
@@ -1,24 +1,53 @@
 import React, { Component } from 'react';
+import Login from 'components/molecules/login';
 import './css/app.css';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
+      username: "root",
+      password: "root",
+      typedUsername: "",
+      typedPassword: "",
       isLoggedIn: false
     };
   }
+
+  handleLogin = ev => {
+    ev.preventDefault();
+    if(this.state.typedUsername === this.state.username && this.state.typedPassword === this.state.password){
+      this.setState({
+        isLoggedIn: true
+      })
+    }else {
+      console.log("bad");
+    }
+  };
+
+  handleTypedUsername = username => {
+    this.setState({
+      typedUsername: username
+    });
+  }
+
+  handleTypedPassword = password => {
+    this.setState({
+      typedPassword: password
+    });
+  }
+
   render() {
     let html = null;
 
     if(this.state.isLoggedIn){
       html = (
-        <div></div>
+        <div>Logged In</div>
       );
     }else {
       html = (
         <div className="o-app__container--child">
-          LOGIN
+          <Login onLogin={this.handleLogin} onTypedUsername={this.handleTypedUsername} onTypedPassword={this.handleTypedPassword}/>
         </div>
       );
     }
