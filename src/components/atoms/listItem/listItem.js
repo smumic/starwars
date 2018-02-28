2884f3c69cbcba2e10a829f2b22f9bc214822ff7
Samir Mumic <mr.samir.mumic@gmail.com>
2018-02-28 19:28:11 +0100

    adds final functiality

--- src/components/atoms/listItem/listItem.js
+++ src/components/atoms/listItem/listItem.js
@@ -3,9 +3,14 @@ import './css/listItem.css';
 
 
 class ListItem extends Component {
+
+  onClick = (e) => {
+    e.target.classList.toggle('selected');
+  }
+
   render(){
     return (
-      <li className="a-listItem a-listItem__item">
+      <li className="a-listItem a-listItem__item" onClick={this.onClick}>
         {
           Object.values(this.props.values).map(function(item, i){
             return (<p className="a-listItem__data-item">{item}</p>);
