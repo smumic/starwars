28d9e8861e8fe6edc3297ebaa19cbe57870fe3bd
Samir Mumic <mr.samir.mumic@gmail.com>
2018-02-28 01:36:22 +0100

    adds atom listItem

---
+++ src/components/atoms/listItem/listItem.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './css/listItem.css';
+
+
+class ListItem extends Component {
+  constructor() {
+    super();
+    this.state = {
+    };
+  }
+
+  render(){
+    return (
+      <li></li>
+    )
+  }
+}
+
+export default ListItem;
