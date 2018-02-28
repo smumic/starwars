8e95952d380f4ef8a2eaf68be35536920efae929
Samir Mumic <mr.samir.mumic@gmail.com>
2018-02-28 12:26:58 +0100

    adds loading molecule

---
+++ src/components/molecules/loading/loading.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './css/loading.css';
+
+
+class Loading extends Component {
+  constructor() {
+    super();
+    this.state = {
+    };
+  }
+  render(){
+    return ()
+  };
+}
+
+export default Loading;
